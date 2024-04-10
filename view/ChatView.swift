27015b53abf2113c2359241f41ea0f80618c9561
Chenhaifeng <cheft@picupmedia.com>
2024-04-10 22:37:34 +0800

    feat: add openai sdk

--- view/ChatView.swift
+++ view/ChatView.swift
@@ -8,12 +8,15 @@
 import SwiftData
 import SwiftUI
 import MarkdownView
+import OpenAI
 
 struct ChatView: View {
     @Environment(\.modelContext) private var context
     
     @Query var setting: [Setting]
     
+    @State private var openai: OpenAIProtocol?
+
     var chatId: Int = 1
     
     //    @Query(filter: #Predicate<Message> { $0.chatId == 1 })
@@ -23,7 +26,9 @@ struct ChatView: View {
     //            SortDescriptor(\Message.time, order: .reverse) // order: .reverse
     //        ]
     //    )
-    @Query var messages: [Message] = []
+//    @Query 
+    
+    @State var messages: [Message] = []
     
     
     @State private var inputText: String = ""
@@ -32,11 +37,11 @@ struct ChatView: View {
     
     init(chatId: Int) {
         self.chatId = chatId
-        let predicate = #Predicate<Message> {
-            $0.chatId == chatId
-        }
-
-        _messages = Query(filter: predicate, sort: \.time) // order: .reverse
+//        let predicate = #Predicate<Message> {
+//            $0.chatId == chatId
+//        }
+//        
+//        _messages = Query(filter: predicate, sort: \.time) // order: .reverse
     }
   
     var body: some View {
@@ -102,15 +107,19 @@ struct ChatView: View {
             .onSubmit {
                 withAnimation {
 //                    tapSendMessage(scrollViewProxy: scrollViewProxy)
-                    sendMessage()
+//                    sendMessage()
                 }
+            }
+            .alert("提示🔔", isPresented: $showAlert) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text("请先设置 apikey 才可以聊天")
             }
             .padding(.leading)
 
             Button(action: {
-                withAnimation {
-//                    tapSendMessage(scrollViewProxy: scrollViewProxy)
-                    sendMessage()
+                Task {
+                    await sendMessage()
                 }
             }) {
                 Image(systemName: "paperplane")
@@ -121,83 +130,123 @@ struct ChatView: View {
                     .padding(.trailing)
             }
             .buttonStyle(PlainButtonStyle())
-            .disabled(inputText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+            .disabled(inputText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || isTextFieldDisabled)
+            
         }
         .padding(.bottom)
     }
     
     
-    func sendMessage() {
+    func sendMessage() async {
         var apiKey: String = ""
-        var proxyUrl: String = ""
+//        var proxyUrl: String = ""
         if !setting.isEmpty {
             apiKey = setting[0].apiKey
-            proxyUrl = setting[0].proxyUrl
+//            proxyUrl = setting[0].proxyUrl
         }
         if (apiKey == "") {
             showAlert = true
             return
+        }
+        if (self.openai == nil) {
+            openai = OpenAI(apiToken: apiKey)
         }
         if inputText.isEmpty || isTextFieldDisabled {
             return
         }
         isTextFieldDisabled = true
         let newMessage = Message(id: UUID().uuidString, chatId: chatId, role: "user", content: inputText, time: Date())
-        context.insert(newMessage)
-        let urlString = "https://api.openai.com/v1/chat/completions"
-        //        let urlString = "http://localhost:11434/api/chat"
+        messages.append(newMessage)
+//        context.insert(newMessage)
         
-        let postData = """
-        {
-            "model": "gpt-4",
-            "temperature": 0,
-            "format": "json",
-            "messages": [
-                {"role": "user", "content": "\(inputText)"}
-            ]
-        }
-        """
-        
-        let customHeaders = [
-            "Authorization": "Bearer \(apiKey)",
-        ]
-        
-        inputText = ""
-        
-        postRequest(urlString: urlString, data: postData, headers: customHeaders, proxyUrl: proxyUrl) { result in
-            switch result {
-            case .success(let data):
-//                if let rawJSONString = String(data: data, encoding: .utf8) {
-//                   print("返回的原始JSON字符串：\(rawJSONString)")
-//                }
-                isTextFieldDisabled = false
-                do {
-                    // 尝试将JSON数据反序列化为Any
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        // 尝试获取choices数组
-                        if let choices = json["choices"] as? [[String: Any]] {
-                            // 获取第一个choice对象
-                            if let firstChoice = choices.first {
-                                // 尝试获取message字典
-                                if let message = firstChoice["message"] as? [String: Any] {
-                                    // 尝试读取content和role
-                                    if let content = message["content"] as? String {
-                                        let msg = Message(id: UUID().uuidString, chatId: chatId, role: "system", content: content, time: Date())
-                                        context.insert(msg)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } catch {
-                    print("解析JSON时发生错误：\(error)")
-                }
-            case .failure(let error):
-                isTextFieldDisabled = false
-                print("Error: \(error)")
-                // 在这里处理错误
+        do {
+            let prompts = messages.map { message in
+                ChatQuery.ChatCompletionMessageParam(role: message.role == "user" ? ChatQuery.ChatCompletionMessageParam.Role.user : ChatQuery.ChatCompletionMessageParam.Role.system, content: message.content)!
             }
+            
+            inputText = ""
+            print(prompts)
+            
+            let chatsStream: AsyncThrowingStream<ChatStreamResult, Error> = openai!.chatsStream(
+                query: ChatQuery(
+                    messages: prompts, model: "gpt-4", maxTokens: 250
+                )
+            )
+            
+            for try await partialChatResult in chatsStream {
+                for choice in partialChatResult.choices {
+                    let messageText = choice.delta.content ?? ""
+                    let msg = Message(id: partialChatResult.id, chatId: chatId, role: "system", content: messageText, time: Date())
+                    if let existingMessageIndex = messages.firstIndex(where: { $0.id == partialChatResult.id }) {
+                        print("update msg \(existingMessageIndex)")
+                        let previousMessage = messages[existingMessageIndex]
+                        messages[existingMessageIndex].content = previousMessage.content + msg.content
+                    } else {
+                        print("new msg \(msg)")
+                        messages.append(msg)
+//                        context.insert(msg)
+                    }
+                }
+            }
+            isTextFieldDisabled = false
+        } catch {
+            print(error)
+            isTextFieldDisabled = false
         }
+        
+        
+//        let urlString = "https://api.openai.com/v1/chat/completions"
+//        //        let urlString = "http://localhost:11434/api/chat"
+//        
+//        let postData = """
+//        {
+//            "model": "gpt-4",
+//            "temperature": 0,
+//            "format": "json",
+//            "messages": [
+//                {"role": "user", "content": "\(inputText)"}
+//            ]
+//        }
+//        """
+//        
+//        let customHeaders = [
+//            "Authorization": "Bearer \(apiKey)",
+//        ]
+//        
+
+//        postRequest(urlString: urlString, data: postData, headers: customHeaders, proxyUrl: proxyUrl) { result in
+//            switch result {
+//            case .success(let data):
+////                if let rawJSONString = String(data: data, encoding: .utf8) {
+////                   print("返回的原始JSON字符串：\(rawJSONString)")
+////                }
+//                isTextFieldDisabled = false
+//                do {
+//                    // 尝试将JSON数据反序列化为Any
+//                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+//                        // 尝试获取choices数组
+//                        if let choices = json["choices"] as? [[String: Any]] {
+//                            // 获取第一个choice对象
+//                            if let firstChoice = choices.first {
+//                                // 尝试获取message字典
+//                                if let message = firstChoice["message"] as? [String: Any] {
+//                                    // 尝试读取content和role
+//                                    if let content = message["content"] as? String {
+//                                        let msg = Message(id: UUID().uuidString, chatId: chatId, role: "system", content: content, time: Date())
+//                                        context.insert(msg)
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                } catch {
+//                    print("解析JSON时发生错误：\(error)")
+//                }
+//            case .failure(let error):
+//                isTextFieldDisabled = false
+//                print("Error: \(error)")
+//                // 在这里处理错误
+//            }
+//        }
     }
 }
-
